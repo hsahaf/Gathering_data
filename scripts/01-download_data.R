4368a569085e96c67f5b3e2ed7b006787ad368b8
Huda Sahaf <hudasahaf27@gmail.com>
2023-02-16 10:46:53 -0500

    Download Data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,17 +1,17 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Huda Sahaf
+# Data: 16 Febuary 2023
+# Contact: huda.sahaf@mail.mail.utoronto.ca 
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Any other information needed? Idk
 
 
-#### Workspace setup ####
-library(opendatatoronto)
+#### Workspace setup ####\
+library(xml2)
+library(httr)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
 # [...ADD CODE HERE TO DOWNLOAD...]
